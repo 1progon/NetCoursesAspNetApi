58b5fd9cf8184794de573fb2e046a0af53477e10
Anton <23635473+1progon@users.noreply.github.com>
2022-10-20 11:24:22 +0400

    Add prop Logo image path

--- NetCourses/Models/Companies/Company.cs
+++ NetCourses/Models/Companies/Company.cs
@@ -5,4 +5,5 @@ public class Company : BaseModel
     public string? Url { get; set; }
     public int? Foundation { get; set; }
     public string? Country { get; set; }
+    public string? LogoImagePath { get; set; }
 }
