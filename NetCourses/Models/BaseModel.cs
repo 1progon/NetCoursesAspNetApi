57797cf227d3239a719cfbde3d556cc4a0479722
Anton <23635473+1progon@users.noreply.github.com>
2022-10-20 11:21:26 +0400

    Remove required unique Slug from BaseModel

--- NetCourses/Models/BaseModel.cs
+++ NetCourses/Models/BaseModel.cs
@@ -1,15 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.EntityFrameworkCore;
 
 namespace NetCourses.Models;
 
-[Index(nameof(Slug), IsUnique = true)]
 public class BaseModel
 {
     [Key] public virtual long Id { get; set; }
 
     [Required] public string Name { get; set; } = null!;
-    [Required] public string Slug { get; set; } = null!;
 
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
