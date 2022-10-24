4adca17fbc8ebcf99b8524c107c230750d1bd72b
Anton <23635473+1progon@users.noreply.github.com>
2022-10-24 11:42:28 +0400

    Add Job Status enum

---
+++ NetCourses/Enums/Jobs/JobStatus.cs
@@ -0,0 +1,15 @@
+namespace NetCourses.Enums.Jobs;
+
+public enum JobStatus
+{
+    Undefined = 0,
+    Moderation = 1,
+    DeclinedModeration = 2,
+    Active = 3,
+    BlockedByAdmin = 4,
+    BlockedByUser = 5,
+    ArchivedByAdmin = 6,
+    ArchivedByUser = 7,
+    DeletedByAdmin = 8,
+    DeletedByUser = 9,
+}
